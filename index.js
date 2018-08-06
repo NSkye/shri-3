27b2ccafbad8e3be8d99347d722f1cee6dd16ffa
Nikolai <nikolai.laevskii@yandex.ru>
2018-08-06 06:29:36 +0300

    Созданы начальные тесты

---
+++ index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+function createSchedule(data) {
+    return;
+}
+
+module.exports = createSchedule;
---
+++ index.spec.js
@@ -0,0 +1,19 @@
+const createSchedule = require('./index.js');
+
+describe('static testing', () => {
+    test('original', () => {
+        expect(createSchedule(require('./data/original.json'))).toEqual({
+            value: 38.939,
+            devices: {
+                '02DDD23A85DADDD71198305330CC386D': 5.398,
+                '1E6276CC231716FE8EE8BC908486D41E': 5.398,
+                'F972B82BA56A70CC579945773B6866FB': 5.1015,
+                '7D9DC84AD110500D284B33C82FE6E85E': 1.5215,
+                'C515D887EDBBE669B2FDAC62F571E9E9': 21.52
+            }
+        });
+    });
+    test('push to limit', () => {
+        expect(() => createSchedule(require('./data/push-to-limit-1.json'))).not.toThrowError();
+    });
+});
